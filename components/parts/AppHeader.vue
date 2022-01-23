6544ebecf2cb7cff978c20e2034df8ec68958b90
harutotanabe <harutotanabe@harutotanabenoMacBook-Air.local>
2022-01-23 16:44:11 +0900

    Crate page

--- components/parts/AppHeader.vue
+++ components/parts/AppHeader.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <h1>table page</h1>
+    <h1 class="main">About Me</h1>
+    <h2 class="description">Server-side engineer</h2>
+    <hr/>
+    <h3>
+        <span class="label">Name:</span>
+        <span class="value">Haruto Tanabe</span>
+    </h3>
+    <h3>
+        <span class="label">Birth:</span>
+        <span class="value">1989/1/11</span>
+    </h3>
+    <h3>
+        <span class="label">Address:</span>
+        <span class="value">Tokyo</span>
+    </h3>
+    <h3>
+        <span class="label">Email:</span>
+        <span class="value">tana167@yahoo.co.jp</span>
+    </h3>
+    <h3>
+        <span class="label">Qiita:</span>
+        <span class="value">https://qiita.com/haruto167</span>
+    </h3>
+    <h3>
+        <span class="label">Github:</span>
+        <span class="value">https://github.com/harutotanabe09</span>
+    </h3>
+    <hr/>
   </div>
 </template>
 
@@ -11,7 +38,11 @@ export default {
 </script>
 
 <style scoped>
-.header {
-  height: 180px;
+.main {
+  color: black;
+}
+
+.description {
+  color: gray;
 }
 </style>
